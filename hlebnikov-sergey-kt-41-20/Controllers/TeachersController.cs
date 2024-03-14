d995851857dc313b405793448551f2a7d30282b0
Jlp0cTaK <viktor28882@gmail.com>
2024-03-14 23:14:34 +0300

    Example

---
+++ hlebnikov-sergey-kt-41-20/Controllers/TeachersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using hlebnikov_sergey_kt_41_20.Data;
+using hlebnikov_sergey_kt_41_20.Models;
+using Microsoft.AspNetCore.Mvc;
+using hlebnikov_sergey_kt_41_20.Interfaces.TeachersInterfaces;
+using hlebnikov_sergey_kt_41_20.Filters.TeachersFilters;
+
+namespace hlebnikov_sergey_kt_41_20.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeachersController : ControllerBase
+    {
+        private readonly ILogger<TeachersController> _logger;
+        private readonly ITeachersService _teachersService;
+
+        public TeachersController(ILogger<TeachersController> logger, ITeachersService teachersService)
+        {
+            _logger = logger;
+            _teachersService = teachersService;
+        }
+
+        [HttpPost(Name = "GetTeachersByDepartments")]
+        public async Task<IActionResult> GetTeachersByDepartmentsAsync(TeachersDepartmentFilter filter, CancellationToken cancellationToken = default)
+        {
+            var teachers = await _teachersService.GetTeachersByDepartmentsAsync(filter, cancellationToken);
+            throw new Exception("1123123");
+            return Ok(teachers);
+        }
+
+
+    }
+}
---
+++ hlebnikov-sergey-kt-41-20/Models/Departments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace hlebnikov_sergey_kt_41_20.Models
+{
+    public class Departments
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public ICollection <Teachers>? Teacher { get; set; }
+        public bool isValidDepartmentName()
+        {
+            return Regex.Match(DepartmentName, @"[A-Z ]+").Success;
+        }
+    }
+}
